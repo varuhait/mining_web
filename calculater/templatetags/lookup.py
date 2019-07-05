151cdcfd9f1f8b27b252804436938cda3038ae1b
Your Name <kasiusuburaito123@gmail.com>
2019-07-05 20:19:24 +0900

    ore_price

--- calculater/templatetags/lookup.py
+++ calculater/templatetags/lookup.py
@@ -4,9 +4,7 @@ register = template.Library()
 
 @register.filter(name='lookup')
 def lookup(value, arg, default=""):
-    if arg in value and type(value[arg]) is int:
+    if arg in value:
         return round(value[arg])
-    elif arg in value and type(value[arg]) is str:
-        return value[arg]        
     else:
         return default
