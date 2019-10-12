f4cc169ff063ab8454656888b3c5186da715abe9
Your Name <kasiusuburaito123@gmail.com>
2019-10-13 00:46:08 +0900

    APIusing 1.08

--- calculater/scripts/setting.py
+++ calculater/scripts/setting.py
@@ -1,7 +1,4 @@
 #スプレッドシートAPI
-import sys
-sys.path.append('/home/oncbnminers/.local/lib/python2.7/site-packages')
-sys.path.append('/usr/local/lib/python2.7/dist-packages')
 import gspread
 import json
 #ServiceAccountCredentials：Googleの各サービスへアクセスできるservice変数を生成します。
